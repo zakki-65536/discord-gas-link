a5edde05f4eeb7ea3aeac122276da891c80b0f6b
zakki-65536 <90125611+zakki-65536@users.noreply.github.com>
2023-03-04 17:53:23 +0900

    別サイトのスクリプトに変更
    https://qiita.com/yuto0214w/items/1ecee25efca6b5b7445b

--- server.js
+++ server.js
@@ -1,64 +1,21 @@
-const http = require('http');
-const querystring = require('querystring');
-const discord = require('discord.js');
-const client = new discord.Client();
-
-http.createServer(function(req, res){
-  if (req.method == 'POST'){
-    var data = "";
-    req.on('data', function(chunk){
-      data += chunk;
-    });
-    req.on('end', function(){
-      if(!data){
-        res.end("No post data");
-        return;
-      }
-      var dataObject = querystring.parse(data);
-      console.log("post:" + dataObject.type);
-      if(dataObject.type == "wake"){
-        console.log("Woke up in post");
-        res.end();
-        return;
-      }
-      res.end();
-    });
-  }
-  else if (req.method == 'GET'){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Discord Bot is active now\n');
-  }
-}).listen(3000);
-
-client.on('ready', message =>{
-  console.log('Bot準備完了～');
-  client.user.setPresence({ activity: { name: 'げーむ' } });
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({
+  intents: Object.values(GatewayIntentBits).reduce((a, b) => a | b)
 });
 
-client.on('message', message =>{
-  if (message.author.id == client.user.id || message.author.bot){
-    return;
-  }
-  if(message.isMemberMentioned(client.user)){
-    sendReply(message, "呼びましたか？");
-    return;
-  }
-  if (message.content.match(/にゃ～ん|にゃーん/)){
-    let text = "にゃ～ん";
-    // sendMsg(message.channel.id, text);
-    sendReply(message, text);
-    return;
-  }
-  
+client.on("ready", () => {
+  console.log(`${client.user.tag} でログインしています。`);
 });
 
-if(process.env.DISCORD_BOT_TOKEN == undefined){
- console.log('DISCORD_BOT_TOKENが設定されていません。');
- process.exit(0);
-}
+client.on("messageCreate", async msg => {
+  if (msg.content === "!ping") {
+    msg.reply("Pong!");
+  }
+});
 
 client.login( process.env.DISCORD_BOT_TOKEN );
 
+/*
 function sendReply(message, text){
   message.reply(text)
     .then(console.log("リプライ送信: " + text))
@@ -70,3 +27,4 @@ function sendMsg(channelId, text, option={}){
     .then(console.log("メッセージ送信: " + text + JSON.stringify(option)))
     .catch(console.error);
 }
+*/
